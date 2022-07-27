ccad19fa163759793ae6983b468ca3e4de3859aa
deigo siahaan <deigosiahaan@gmail.com>
2022-07-27 13:40:34 +0700

    add swagger

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const movieRouter = require('./routes/movies')
 const port = 9000
-
 const app = express()
+
+// swagger
+const swagger = require('swagger-ui-express')
+const apiDocumentation = require('./apidocs.json')
+app.use('/api-docs', swagger.serve, swagger.setup(apiDocumentation))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
